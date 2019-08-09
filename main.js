2b5a84ad0ee0e800204ef2c0b00d9d25c771ba8a
davinbirdi <davin.birdi@hotmail.ca>
2019-08-08 18:32:44 -0700

    Skeleton Login/Register Start Page

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow () {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile('login.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
