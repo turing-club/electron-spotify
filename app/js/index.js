e6db2a9341ee6ace7eaafc07120233ecd7474cfa
Pablo Arellano <pablo.arellanos97@gmail.com>
2019-08-13 23:40:46 -0700

    123

--- app/js/index.js
+++ app/js/index.js
@@ -1,27 +1,140 @@
-'use strict'
+'use strict';
 
-// Button Toggle
-document.getElementById('login-reg-button').addEventListener('click', () => {
+const { BrowserWindow } = require('electron').remote;
+let csrftoken = Cookies.get('csrftoken');
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+});
+
+
+$(document).on("click", ".register", function(event){
   let status = document.getElementById('login-reg-button');
-  if(status.firstElementChild.id == 'register'){
-    status.innerHTML='<a id="login" href="#">Log In</a>';
-    showForm();
-  }
-  else{
-    status.innerHTML='<a id="register" href="#">Sign Up</a>';
-    showForm();
-  }
+  status.innerHTML='<a class="login" href="#">Login</a>';
+  showForm('login');
+});
+
+$(document).on("click", ".login", function(event){
+    let status = document.getElementById('login-reg-button');
+    status.innerHTML='<a class="register" href="#">Sign Up</a>';
+    showForm('register');
 });
 
 // Function to Show or Hide the login/register forms on the landing page
-function showForm(){  
-  let status = document.getElementById('login-reg-button').firstChild.id;
-  if(status == 'login'){
+function showForm(status){
+  if (status === 'login'){
     document.getElementById('register-form').style="display:block";
     document.getElementById('login-form').style="display:none";
   }
-  else{
+  else {
     document.getElementById('login-form').style="display:block";
     document.getElementById('register-form').style="display:none";
   }
 }
+
+function sendError() {
+    document.getElementById('error').innerHTML = '  <div class="alert alert-warning alert-dismissible fade show" role="alert">\n' +
+        '      <strong>Holy guacamole!</strong> You should check in on some of those fields below.\n' +
+        '      <button type="button" class="close" data-dismiss="alert" aria-label="Close">\n' +
+        '        <span aria-hidden="true">&times;</span>\n' +
+        '      </button>\n' +
+        '    </div>';
+}
+async function openSpotifyWindow(userID) {
+    console.log('clicked');
+    let authWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+        'node-integration': false,
+        'web-security': false
+    });
+    let authUrl = 'https://accounts.spotify.com/authorize?response_type=code&client_id=baa5f3746bf74710bf3f6b18926993db&redirect_uri=http://127.0.0.1:8000/setup-success';
+
+    await authWindow.loadURL(authUrl);
+    authWindow.show();
+    authWindow.webContents.openDevTools();
+    authWindow.webContents.on('did-redirect-navigation', function (event, newUrl) {
+        event.preventDefault();
+        console.log(newUrl);
+        if (newUrl.includes('code=')) {
+            let code = newUrl.split('code=')[1];
+            authWindow.close();
+            addCodeToUser(code, userID);
+            window.location.href = "home.html";
+        } else {
+            authWindow.close();
+            sendError();
+        }
+    });
+
+    authWindow.on('closed', function() {
+        authWindow = null;
+    });
+}
+
+function addCodeToUser(code, userID) {
+    $.ajax({
+        type: 'POST',
+        url: "http://127.0.0.1:8000/api/add-code",
+        data: {
+            code : code,
+            user_id: userID,
+        },
+        success: function(result) {
+            console.log(result);
+        }
+    });
+}
+
+document.getElementById('register-form-button').addEventListener('click', (e) => {
+    e.preventDefault();
+   let username = document.getElementById('register-username').value;
+   let password = document.getElementById('register-password').value;
+   let email = document.getElementById('register-email').value;
+    $.ajax({
+        type: 'POST',
+        url: "http://127.0.0.1:8000/api/register",
+        data: {
+            username : username,
+            password: password,
+            email: email,
+        },
+        success: async function(result) {
+            console.log(result);
+            if (result.status === 200) {
+                localStorage.setItem('userID', result.user_id);
+                await openSpotifyWindow(result.user_id);
+            } else {
+                sendError();
+            }
+        }
+    });
+   return false;
+});
+
+document.getElementById('login-form-button').addEventListener('click', (e) => {
+    e.preventDefault();
+    let username = document.getElementById('log-in-username').value;
+    let password = document.getElementById('log-in-password').value;
+    $.ajax({
+        type: 'POST',
+        url: "http://127.0.0.1:8000/api/login",
+        data: {
+            username : username,
+            password: password,
+        },
+        success: async function(result) {
+            console.log(result);
+            if (result.status === 200) {
+                localStorage.setItem('userID', result.user_id);
+                window.location.href = "home.html";
+            } else {
+                sendError();
+                return false;
+            }
+        }
+    });
+    return false;
+});
--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
-// Use electron-reload that allows hot-reloading of browsers when changes are made
 const electron = require('electron')
 require('electron-reload')(__dirname);
+
 
 app.commandLine.appendSwitch('no-verify-widevine-cdm');
 
@@ -13,46 +12,50 @@ app.commandLine.appendSwitch('no-verify-widevine-cdm');
 let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    }
-  });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true
+        }
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('app/index.html');
+    mainWindow.setResizable(true);
 
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+    // and load the index.html of the app.
+    mainWindow.loadFile('app/index.html');
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
 // Set up Widevine (DRM engine required to play Spotify songs in Electron - Chrome has it by default but Electron does
 // not, so we have to load it manually).
 app.on('ready', () => {
-  // Demonstrating with default session, but a custom session object can be used
-  app.verifyWidevineCdm({
-    session: BrowserWindow.defaultSession,
-  });
+    // Demonstrating with default session, but a custom session object can be used
+    app.verifyWidevineCdm({
+        session: BrowserWindow.defaultSession,
+    });
+
+    // Do other early initialization...
 });
 
 app.on('widevine-ready', (version, lastVersion) => {
-  console.log(version);
-  if (null !== lastVersion) {
-    console.log('Widevine ' + version + ', upgraded from ' + lastVersion + ', is ready to be used!');
-  } else {
-    console.log('Widevine ' + version + ' is ready to be used!');
-  }
+    console.log(version);
+    if (null !== lastVersion) {
+        console.log('Widevine ' + version + ', upgraded from ' + lastVersion + ', is ready to be used!');
+    } else {
+        console.log('Widevine ' + version + ' is ready to be used!');
+    }
 });
 
 // This method will be called when Electron has finished
@@ -62,15 +65,15 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
 });
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
 });
 
 // In this file you can include the rest of your app's specific main process
