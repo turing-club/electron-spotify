a66c58d306fcf342c90a091d00642c741184da32
Brandon Forys <bforys@gmail.com>
2019-08-20 18:38:12 -0700

    Push frontend playlist button

--- app/js/home.js
+++ app/js/home.js
@@ -10,3 +10,19 @@ function generatePlaylist(userID) {
         }
     });
 }
+
+document.getElementById('get-playlists-button').addEventListener('click', (e) => {
+    e.preventDefault();
+    userID = localStorage.getItem('userID');
+    $.ajax({
+        type: 'GET',
+        url: "http://127.0.0.1:8000/api/get-playlists",
+        data: {
+            user_id: userID,
+        },
+        success: function (result) {
+            console.log(result);
+        }
+    });
+    return false;
+});
