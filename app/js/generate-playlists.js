317b6b7ad5a469aeb7a4a1ce3d783f367ea23af4
ad2969 <Clarenceadrian@gmail.com>
2019-09-08 22:21:14 -0700

    add filtering

--- app/js/generate-playlists.js
+++ app/js/generate-playlists.js
@@ -8,22 +8,47 @@ const playlistDiv = document.getElementById('playlist-choice-list');
 // NAVIGATION BUTTONS
 ////////////////////////////////////////
 
+document.getElementById('logout-button').addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.clear();
+  window.location.href = "index.html";
+});
+
 document.getElementById('my-playlists-button').addEventListener("click", function(){
     window.location.href = "home.html";
 });
 
 document.getElementById('proceed-button').addEventListener("click", function(){
+    var num_songs = document.getElementById('num_songs').value
+    if (!num_songs) {
+      alert("Enter number of songs!");
+      return;
+    } // User must enter number of songs
+
+    let playlist_array = arrayFilter(selectedPlaylists);
+
     let playlist_title = document.getElementById('name_playlist').value;
     if (playlist_title) title = playlist_title;
     else title = "Untitled Playlist";
 
+    console.log("Rate Songs for Playlist:", title, "!");
+    console.log("Filtered List: ", playlist_array);
+
+    userID = localStorage.getItem('userID');
+    
+
 });
 
-document.getElementById('logout-button').addEventListener('click', (e) => {
-    e.preventDefault();
-    localStorage.clear();
-    window.location.href = "index.html";
-});
+// MISC
+////////////////////////////////////////
+
+function arrayFilter(array) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++ ) {
+      if (array[i] !== "") newArray.push(array[i]);
+    }
+    return newArray;
+}
 
 
 // GENERATE
@@ -64,9 +89,9 @@ function listPlaylist() {
             // console.log(result_playlist);
             if (selectedPlaylists) {
               if (selectedPlaylists.length !== result_playlists.playlists.length)
-                selectedPlaylists = new Array(result_playlists.playlists.length).fill(0);
+                selectedPlaylists = new Array(result_playlists.playlists.length).fill("");
             }
-            else selectedPlaylists = new Array(result_playlists.playlists.length).fill(0);
+            else selectedPlaylists = new Array(result_playlists.playlists.length).fill("");
             clearPlaylist();
               for( let i = 0; i < result_playlists.playlists.length; i++ ) {
                 loadPlaylist(result_playlists.playlists[i], i);
@@ -77,17 +102,18 @@ function listPlaylist() {
 
 function selectClick(event) {
     event.preventDefault();
-    // console.log(event.srcElement.parentElement.dataset.index);
+    // console.log(event.srcElement.parentElement.dataset.playlistid);
     var isElement = event.srcElement.parentElement.className.includes('playlist_element');
     var isHighlighted = event.srcElement.parentElement.className.includes('choice--highlighted');
 
     if(!isElement) return;
+    var playlist_id = event.srcElement.parentElement.id.replace('playlist-','');
     if(isHighlighted) {
-      selectedPlaylists[event.srcElement.parentElement.dataset.index] = 0;
+      selectedPlaylists[event.srcElement.parentElement.dataset.playlistid] = "";
       event.srcElement.parentElement.setAttribute('class', 'playlist_element mb-0');
     }
     else {
-      selectedPlaylists[event.srcElement.parentElement.dataset.index] = 1;
+      selectedPlaylists[event.srcElement.parentElement.dataset.playlistid] = playlist_id;
       event.srcElement.parentElement.setAttribute('class', 'playlist_element mb-0 choice--highlighted');
     }
     // console.log(selectedPlaylists);
@@ -96,32 +122,32 @@ function selectClick(event) {
 // Generates an Unordered List of the Playlists given the API data for "api/get-playlists"
 function loadPlaylist(data, index){
       // Name of the Playlist in the list of playlists
-      var playlist_title = data.name;
+    var playlist_title = data.name;
 
-      // Creating the HTML Elements
-      var list_element = playlistDiv.appendChild(document.createElement("tr"));
-      var playlist_img = list_element.appendChild(document.createElement("td"));
-      var playlist_img__img = playlist_img.appendChild(document.createElement("img"));
-      var playlist_name = list_element.appendChild(document.createElement("td"));
+    // Creating the HTML Elements
+    var list_element = playlistDiv.appendChild(document.createElement("tr"));
+    var playlist_img = list_element.appendChild(document.createElement("td"));
+    var playlist_img__img = playlist_img.appendChild(document.createElement("img"));
+    var playlist_name = list_element.appendChild(document.createElement("td"));
 
-      playlist_name.textContent = playlist_title;
+    playlist_name.textContent = playlist_title;
 
-      try {
-        playlist_img__img.setAttribute('src', data.images[0].url);
-      }
-      catch (error) {
-        console.log("Error loading thumbnail for song: ", data.name);
-        playlist_img__img.setAttribute('src', '../assets/defaultsong.png');
-      }
-      playlist_img__img.setAttribute('class', 'playlist-choice__img');
+    try {
+      playlist_img__img.setAttribute('src', data.images[0].url);
+    }
+    catch (error) {
+      console.log("Error loading thumbnail for song: ", data.name);
+      playlist_img__img.setAttribute('src', '../assets/defaultsong.png');
+    }
+    playlist_img__img.setAttribute('class', 'playlist-choice__img');
 
 
-      // Asserting Class and ID attributes to each element
-      if( selectedPlaylists[index] ) list_element.setAttribute('class', 'playlist_element mb-0 choice--highlighted');
-      else list_element.setAttribute('class', 'playlist_element mb-0');
-      list_element.setAttribute('data-index', index);
-      list_element.setAttribute('id', 'playlist-' + data.id);
-      list_element.addEventListener("click", selectClick);
+    // Asserting Class and ID attributes to each element
+    if( selectedPlaylists[index] ) list_element.setAttribute('class', 'playlist_element mb-0 choice--highlighted');
+    else list_element.setAttribute('class', 'playlist_element mb-0');
+    list_element.setAttribute('data-playlistid', index);
+    list_element.setAttribute('id', 'playlist-' + data.id);
+    list_element.addEventListener("click", selectClick);
 
-      playlistDiv.appendChild(list_element);
+    playlistDiv.appendChild(list_element);
 }
